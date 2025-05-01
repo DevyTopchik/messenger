52f307610fc722f016b0bb9ccdc519671cdbcebf
Dzianis Nichyparovich <krocodilegenatop@gmail.com>
2025-05-01 21:39:37 +0300

    user data apis

---
+++ src/api/change_user_name.js
@@ -0,0 +1,22 @@
+export const changeUserName = (id, newUserName) => {
+  return fetch("https://textchat-ast1.onrender.com/api/auth/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      id: id,
+      newUserName: newUserName,
+    }),
+  })
+    .then((response) => {
+      if (!response.ok) {
+        return response.json().then((err) => Promise.reject(err));
+      }
+      return response.json();
+    })
+    .catch((e) => {
+      console.log(e);
+      return Promise.reject(e);
+    });
+};
---
+++ src/api/get_user_data.js
@@ -0,0 +1,21 @@
+export const getUserData = (id) => {
+  return fetch("https://textchat-ast1.onrender.com/api/auth/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      id: id,
+    }),
+  })
+    .then((response) => {
+      if (!response.ok) {
+        return response.json().then((err) => Promise.reject(err));
+      }
+      return response.json();
+    })
+    .catch((e) => {
+      console.log(e);
+      return Promise.reject(e);
+    });
+};
